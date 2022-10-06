d8268b8e857a1d1193853f15f6105e9ddc1bdca8
mpuchyr <matt.puchyr@gmail.com>
2022-10-06 16:18:10 +0000

    Set up routes for blank_square_root_form.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
 
+  def blank_square_root_form
+    render({ :template => "calculation_templates/blank_square_root_form.html.erb"})
+  end
+
   def blank_random_form
     render({ :template => "calculation_templates/random_form.html.erb"})
   end
